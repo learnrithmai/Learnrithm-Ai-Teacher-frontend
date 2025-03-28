55f011743f6e00994fc42e664cccce1d84de0d1c
iayvob <iayvob@gmail.com>
2025-03-29 00:03:23 +0100

    update the urls

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,8 +1,8 @@
 // Define the URL of the client and server  
 
 export const CLIENT_URL = "https://app.learnrithm.com/"
-export const SERVER_URL = "https://backend.learnrithm.com"
-export const SERVER_API_URL = "https://backend.learnrithm.com/api/v2"
+export const SERVER_URL = "https://learnrithm-backend.onrender.com/"
+export const SERVER_API_URL = "https://learnrithm-backend.onrender.com/api/v2"
 
 
 // Define the send email types
